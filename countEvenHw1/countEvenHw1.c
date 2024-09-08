9be1c3a9548f6c5eb9ee42e5e42f29aa44f1ec2c
James Kim <yt4921@gmail.com>
2024-09-08 11:41:44 -0700

    create isOddPalindrome function that returns bool

--- countEvenHw1/countEvenHw1.c
+++ countEvenHw1/countEvenHw1.c
@@ -1,22 +1,50 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+bool is_odd_palindrome(const char* str);
 
 int main() {
 
 	long x = -11;
 	long y = 8;
 
-	const int arr[] = {1,2,3,4,1};
+	const int arr[] = {1,2,3,3,4};
 	int n = 5;
 
+	const char str[] = "abcba";
+
+
 	int evenArr = count_even(x, y);
-	printf("Number of the even numbers are: %d", evenArr);
+	printf("Number of the even numbers are: %d\n", evenArr);
 
 	int mostFrequentNum = most_frequent(arr, n);
-	printf("The most frequent number is: %d", mostFrequentNum);
+	printf("The most frequent number is: %d\n", mostFrequentNum);
+
+	bool isArrOddPalindrome = is_odd_palindrome(str);
+	printf("%d", isArrOddPalindrome);
 
 	return 0;
 }
 
+bool is_odd_palindrome(const char* str) {
+	int length = strlen(str);
+
+	int highestNum = length - 1;;
+	int lowestNum = 0;
+
+	if (length % 2 == 0) {
+		return false;
+	}
+	while (lowestNum < highestNum) {
+		if (str[lowestNum] != str[highestNum]) {
+			return false;
+		}
+		lowestNum++;
+		highestNum--;
+	}
+	return true;
+
+}
 
 
 int most_frequent(const int* arr, int n) {
