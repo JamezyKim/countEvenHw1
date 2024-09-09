3996a4644e9b3dc32fc629f1aea924538a9a127e
James Kim <yt4921@gmail.com>
2024-09-08 18:29:08 -0700

    create void function strDivByDigit

--- countEvenHw1/countEvenHw1.c
+++ countEvenHw1/countEvenHw1.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool is_odd_palindrome(const char* str);
+//bool is_odd_palindrome(const char* str);
+
+void str_div_by_digit(const char* num, int digit);
 
 int main() {
 
@@ -20,31 +22,61 @@ int main() {
 	int mostFrequentNum = most_frequent(arr, n);
 	printf("The most frequent number is: %d\n", mostFrequentNum);
 
-	bool isArrOddPalindrome = is_odd_palindrome(str);
-	printf("%d", isArrOddPalindrome);
+	//longest_odd_subpalindrome(str);
+
+	const char* num = "15";
+	int digit = 5;
+	str_div_by_digit(num, digit);
 
 	return 0;
 }
 
-bool is_odd_palindrome(const char* str) {
-	int length = strlen(str);
+void str_div_by_digit(const char* num, int digit) {
 
-	int highestNum = length - 1;;
-	int lowestNum = 0;
+	
+	int numOfStr = atoi(num);
+	int quotient = numOfStr / digit;
+	int remainder = numOfStr % digit;
 
-	if (length % 2 == 0) {
-		return false;
-	}
-	while (lowestNum < highestNum) {
-		if (str[lowestNum] != str[highestNum]) {
-			return false;
-		}
-		lowestNum++;
-		highestNum--;
-	}
-	return true;
 
+
+	printf("%d R %d", quotient, remainder);
+	return;
 }
+
+//int longest_odd_subpalindrome(const char* str) {
+//	int length = strlen(str);
+//	int reverseCounter = length;
+//	int palindromeCounter = 0;
+//
+//
+//	for (int i = 0; i < length; i++) {
+//		for (int j = i; j < length; j++) {
+//			if (is_odd_palindrome(str,i,j)) {
+//				return 1;
+//			}
+//			
+//		}
+//	}
+//}
+
+//bool is_odd_palindrome(const char* str, int lowestNum,int highestNum) {
+//	int length = strlen(str);
+//
+//
+//	if (length % 2 == 0) {
+//		return false;
+//	}
+//	while (lowestNum < highestNum) {
+//		if (str[lowestNum] != str[highestNum]) {
+//			return false;
+//		}
+//		lowestNum++;
+//		highestNum--;
+//	}
+//	return true;
+//
+//}
 
 
 int most_frequent(const int* arr, int n) {
